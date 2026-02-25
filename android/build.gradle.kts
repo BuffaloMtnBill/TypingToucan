18126a6348caa8b29e32df7ec16e04edc5063634
BuffaloMtnBill <64105445+BuffaloMtnBill@users.noreply.github.com>
2026-02-24 21:50:50 -0500

    Bump version to 0.9.1 (versionCode 3)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.typingtoucan.android"
         minSdk = 21
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.9.0"
+        versionCode = 3
+        versionName = "0.9.1"
     }
 
     signingConfigs {
